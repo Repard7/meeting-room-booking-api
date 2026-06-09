0a27374d2976cf1e359ba899565b187fb480c5ea
Ростислав Грачев <gracevrostislav5@gmail.com>
2026-06-09 16:09:49 +0700

    chore: init commit

---
+++ src/main/java/dev/grachev/meeting_room_booking_api/MeetingRoomBookingApiApplication.java
@@ -0,0 +1,13 @@
+package dev.grachev.meeting_room_booking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomBookingApiApplication.class, args);
+	}
+
+}
